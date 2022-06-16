37dc0d601046ba1887754f178f5549d0bd916e50
ezez-refer <jjn00712@gmail.com>
2022-06-16 15:23:58 +0900

    feat: models, preprocess, data_processing 수정에 따른 실행 코드 수정
    - flag 인자 수정(작업을 구분하여 실행할 수 있도록 수정함)
    	- 0: 데이터 전처리(결측치 제거, 보간 등 처리), 학습용 데이터셋 구축, 학습 및 추론, 결과 저장
    	- 1: 학습용 데이터셋 구축, 학습 및 추론, 결과 저장
    	- 2: 학습 및 추론, 결과 저장

--- main.py
+++ main.py
@@ -1,22 +1,52 @@
 from data_processing import cipDataset
+from preprocess import preprocess
+from dataloader import dataload
+from utils import saveDataFrame
+from models import Models
 import argparse
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='dataset')
     parser.add_argument(
-        '-load_path', help='insert data path and filename, dir/filename', default=None, type=str)
+        '-load_path', help='insert data path and filename, dir/filename', default='./data/', type=str)
     parser.add_argument(
-        '-save_path', help='insert save path and filename, dir/filename', default=None, type=str)
+        '-save_path', help='insert save path and filename, dir/filename', default='./processing_result/', type=str)
     parser.add_argument(
-        '-flag', help='select data to process, "train" or "test"', default=None, type=str)
+        '-flag', help='select preprocess to train or build data to train or just train, "0" or "1" or "2"', default=0, type=int)
     parser.add_argument(
-        '-logs', help='insert logs path and filename, dir/filename', default=None, type=str)
+        '-logs', help='insert logs path and filename, dir/filename', default='./logs/', type=str)
     args = parser.parse_args()
 
     # ex) load_path = 'cip/EDA/preprocessing/'           # 데이터 불러오는 경로
     # ex) save_path = 'cip/EDA/processing_result/'       # 데이터 처리 후 데이터 저장하는 경로
 
-    data = cipDataset(args.load_path, args.save_path)
-    
-    train_err_data = data.makeFeatures(args.flag)
-    data.saveDataFrame(train_err_data)
+    if args.flag == 1:
+        train_err = dataload("train error 데이터")
+        train_qual = dataload("train quality 데이터")
+        train_prob = dataload("train problem 데이터")
+        test_err = dataload("test error 데이터")
+        test_qual = dataload("test quality 데이터")
+
+        preproc = preprocess(train_err, train_qual, train_prob, test_err, test_qual)
+
+    if args.flag <= 1:
+        train_data = dataload('전처리된 train error 데이터')
+        test_data = dataload('전처리된 test error 데이터')
+        data = cipDataset(train_data, test_data, args.load_path, args.save_path)
+        train_dataset = data.makeFeatures(train_data, 'train')
+        test_dataset = data.makeFeatures(test_data, 'test')
+        saveDataFrame(args.save_path, train_dataset)
+        saveDataFrame(args.save_path, test_dataset)
+
+        model = Models(train_dataset, test_dataset)
+        print(model.LGBM())
+        model.inference()
+
+    else:
+        train_dataset = dataload('train 데이터셋')
+        test_dataset = dataload('test 데이터셋')
+        
+        model = Models(train_dataset, test_dataset)
+        print(model.LGBM())
+        model.inference()
