c42cd0e8d9dd7d9004638df723850ad262f91ea3
ezez-refer <jjn00712@gmail.com>
2022-06-16 20:28:49 +0900

    feat: LightGBM 및 코드 정리, RandomForest, XGBoost, XGBRF, LinearRegression 모델 추가
    - 각 모델 optuna 적용, k-fold 적용, 학습, 추론, 결과 저장 기능 구현
    - k-fold 메서드 구현
    - Models class 매개변수로 optuna trial 값추가

--- models.py
+++ models.py
@@ -1,23 +1,27 @@
 import pandas as pd
 import numpy as np
-import lightgbm as lgb
 from sklearn.metrics import *
 from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings(action='ignore')
 import optuna
-from optuna import Trial, visualization
 from optuna.samplers import TPESampler
-from utils import saveDataFrame
-from dataloader import dataload
 import lightgbm as lgb
-from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+
+from dataloader import dataload
+from utils import saveDataFrame
 
 
 class Models():
-    def __init__(self, train_dataset, test_dataset):
+    def __init__(self, train_dataset, test_dataset, trial = 10):
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
+        self.trial = trial
+        self.result = None
 
     def inference(self):
         test_err = self.test_dataset
@@ -43,73 +47,20 @@ class Models():
         score=auc(r,p) 
         return score
 
-    def objective(self, trial):
-        train_x = self.train_dataset.iloc[:, 1:-1] 
-        train_y = self.train_dataset.iloc[:,-1] 
-        print(train_x.shape)
-        print(train_y.shape)
-        train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=1)
-        param = {
-            'objective': 'regression', # 회귀
-            'verbose': -1,
-            'metric': 'auc', 
-            'max_depth': trial.suggest_int('max_depth',5, 20),
-            'learning_rate': trial.suggest_loguniform("learning_rate", 1e-8, 0.8),
-            'num_boost_round': trial.suggest_int('num_boost_round',100,1500)
-            # 'n_estimators': trial.suggest_int('n_estimators', 100, 3000),
-            # 'min_child_samples': trial.suggest_int('min_child_samples', 5, 100),
-            # 'subsample': trial.suggest_loguniform('subsample', 0.4, 1),
-        }
+    def k_folding(self, params, select_model):
+        models     = []
+        recalls    = []
+        precisions = []
+        auc_scores   = []
+        threshold = 0.5
 
-        model = lgb.LGBMRegressor(**param)
-        model = model.fit(train_x, train_y, eval_set=[(valid_x, valid_y)], verbose=0, early_stopping_rounds=25)
-        pred = model.predict(valid_x)
-        auc = self.f_pr_auc(pred, valid_y)
-        return auc
-    
-    def LGBM(self):
-        sampler = TPESampler(seed=10)
-        study_lgb = optuna.create_study(direction='maximize', sampler=sampler)
-        study_lgb.optimize(self.objective, n_trials=50)
-        best_params = study_lgb.best_params
-
-        # cross validation 전 초기화
         train_x = self.train_dataset.iloc[:, 1:-1] 
         train_y = self.train_dataset.iloc[:,-1] 
         print("K-fold")
         print(train_x.shape)
         print(train_y.shape)
 
-        # Train
-        #-------------------------------------------------------------------------------------
-        # validation auc score를 확인하기 위해 정의
-        def f_pr_auc(probas_pred, y_true):
-            labels=y_true.get_label()
-            p, r, _ = precision_recall_curve(labels, probas_pred)
-            score=auc(r,p) 
-            return "pr_auc", score, True
-        #-------------------------------------------------------------------------------------
-        models     = []
-        recalls    = []
-        precisions = []
-        auc_scores   = []
-        threshold = 0.5
-        # 파라미터 설정
-        params =      {
-                        'boosting_type'     : 'gbdt',
-                        'objective'         : 'regression',
-                        'metric'            : 'auc',
-                        'seed'              : 1015,
-                        'learning_rate'     : best_params['learning_rate'],
-                        'max_depth'         : best_params['max_depth'],
-                        'num_boost_round'   : best_params['num_boost_round']
-                    # ETC
-                        # 'min_child_samples' : best_params['min_child_samples'],
-                        # 'n_estimators'      : best_params['n_estimators'],
-                        # 'subsample'         : best_params['subsample']
-                        }
-        #-------------------------------------------------------------------------------------
-        # 5 Kfold cross validation
+        # Kfold cross validation
         k_fold = KFold(n_splits=5, shuffle=True, random_state=42)
         for train_idx, val_idx in k_fold.split(train_x):
 
@@ -118,20 +69,14 @@ class Models():
             y = train_y.iloc[train_idx]
             valid_x = train_x.iloc[val_idx]
             valid_y = train_y.iloc[val_idx]
-
-            d_train= lgb.Dataset(X, y)
-            d_val  = lgb.Dataset(valid_x, valid_y)
             
             #run traning
-            model = lgb.train(
-                                params,
-                                train_set       = d_train,
-                                num_boost_round = 1000,
-                                valid_sets      = d_val,
-                                feval           = f_pr_auc,
-                                verbose_eval    = 20, 
-                                early_stopping_rounds = 3,
-                            )
+            model = select_model(**params)
+            if select_model in [lgb.LGBMRegressor, xgb.XGBRegressor, xgb.XGBRFRegressor]:
+                model.fit(X, y, eval_set=[(valid_x, valid_y)], verbose=0)
+            else:
+                model.fit(X,y)
+
             # cal valid prediction
             valid_prob = model.predict(valid_x)
             valid_pred = np.where(valid_prob > threshold, 1, 0)
@@ -147,7 +92,130 @@ class Models():
             precisions.append(precision)
             auc_scores.append(auc_score)
 
-            print('==========================================================')
+            print('============================ k-fold 실행 중 ============================')
 
         self.result = models, recalls, precisions, auc_scores
-        return models, recalls, precisions, auc_scores
+
+
+    def LR(self):
+        params = {}
+        self.k_folding(params, LinearRegression)
+        return self.result
+
+    def LGBM(self):
+        # optuna
+        train_x = self.train_dataset.iloc[:, 1:-1] 
+        train_y = self.train_dataset.iloc[:,-1] 
+        train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=1)
+        sampler = TPESampler(seed=10)
+
+        def objective(trial):
+            param = {
+                'boosting_type'         : 'gbdt',
+                'objective'             : 'regression',
+                'seed'                  : 1015,
+                'verbose'               : -1,
+                'metric'                : 'auc', 
+                'max_depth'             : trial.suggest_int('max_depth',5, 20),
+                'learning_rate'         : trial.suggest_loguniform("learning_rate", 1e-8, 0.8),
+                'num_iterations'        : trial.suggest_int('num_iterations',100,1500),
+            }
+            
+            model = lgb.LGBMRegressor(**param)
+            model = model.fit(train_x, train_y, eval_set=[(valid_x, valid_y)], verbose=0, early_stopping_rounds=25)
+            pred = model.predict(valid_x)
+            auc = self.f_pr_auc(pred, valid_y)
+            return auc
+                
+        study = optuna.create_study(direction='maximize', sampler=sampler)
+        study.optimize(objective, n_trials = self.trial)
+        best_params = study.best_params
+
+        self.k_folding(best_params, lgb.LGBMRegressor)
+        return self.result
+
+    def XGB(self):
+        # optuna
+        train_x = self.train_dataset.iloc[:, 1:-1] 
+        train_y = self.train_dataset.iloc[:,-1] 
+        train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=1)
+        sampler = TPESampler(seed=10)
+
+        def objective(trial):
+            param = {
+                'learning_rate'     : trial.suggest_loguniform("learning_rate", 1e-8, 0.5),
+                'n_estimators'      : trial.suggest_int("n_estimators", 100,1000),
+                'max_depth'         : trial.suggest_int("max_depth",3,8),
+                'min_child_weight'  : trial.suggest_int("min_child_weight", 1, 10),
+                'gamma'             : trial.suggest_int("gamma", 0, 10),
+                'subsample'         : trial.suggest_float("subsample", 0.5, 1),
+            }
+
+            model = xgb.XGBRegressor(**param)
+            model = model.fit(train_x, train_y, eval_set=[(valid_x, valid_y)], verbose=0, early_stopping_rounds=25)
+            pred = model.predict(valid_x)
+            auc = self.f_pr_auc(pred, valid_y)
+            return auc
+
+        study = optuna.create_study(direction='maximize', sampler=sampler)
+        study.optimize(objective, n_trials = self.trial)
+        best_params = study.best_params
+
+        self.k_folding(best_params, xgb.XGBRegressor)
+        return self.result
+
+    def XGBRF(self):
+        # optuna
+        train_x = self.train_dataset.iloc[:, 1:-1] 
+        train_y = self.train_dataset.iloc[:,-1] 
+        train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=1)
+        sampler = TPESampler(seed=10)
+
+        def objective(trial):
+            param = {
+                'max_depth' : trial.suggest_int('max_depth', 5, 15),
+                'learning_rate': trial.suggest_loguniform("learning_rate", 1e-8, 1e-2),
+                'num_parallel_tree': trial.suggest_int('num_parallel_tree',5,100),
+                'n_estimators': trial.suggest_int('n_estimators',50,100),
+            }
+
+            model = xgb.XGBRFRegressor(**param)
+            model = model.fit(train_x, train_y)
+            pred = model.predict(valid_x)
+            auc = self.f_pr_auc(pred, valid_y)
+            return auc
+
+        study = optuna.create_study(direction='maximize', sampler=sampler)
+        study.optimize(objective, n_trials = self.trial)
+        best_params = study.best_params
+
+        self.k_folding(best_params, xgb.XGBRFRegressor)
+        return self.result
+
+    def RF(self):
+        # optuna
+        train_x = self.train_dataset.iloc[:, 1:-1] 
+        train_y = self.train_dataset.iloc[:,-1] 
+        train_x, valid_x, train_y, valid_y = train_test_split(train_x, train_y, test_size=0.2, random_state=1)
+        sampler = TPESampler(seed=10)
+
+        def objective(trial):
+
+            param = {
+                'n_estimators': trial.suggest_int('n_estimators', 15, 100),
+                'max_depth' : trial.suggest_int('max_depth', 9, 20),
+                'min_samples_leaf' : trial.suggest_int('min_samples_leaf', 3, 15),
+                'min_samples_split' : trial.suggest_int('min_samples_split', 5, 20)
+            }
+
+            model = RandomForestRegressor(**param)
+            rf_model = model.fit(train_x, train_y)
+            auc = self.f_pr_auc(rf_model.predict(valid_x),valid_y)
+            return auc
+
+        study = optuna.create_study(direction='maximize', sampler=sampler)
+        study.optimize(objective, n_trials = self.trial)
+        best_params = study.best_params
+
+        self.k_folding(best_params, RandomForestRegressor)
+        return self.result
